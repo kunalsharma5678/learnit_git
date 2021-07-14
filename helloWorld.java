946311abecc2308b0f7daa4b5f156bbc29670558
Kunal Sharma <kunal.sharmaAexample.com>
2021-07-14 16:36:41 +0530

    init commit

---
+++ helloWorld.java
@@ -0,0 +1,28 @@
+package Com.itBulls.kunal;
+
+
+import java.util.Arrays;
+
+
+
+
+
+
+public class helloWorld {
+		public static void main(String[] args) {
+		
+			Account accountFrom = new Account(1);
+			Account accountTo = new Account(2);
+			
+			accountFrom.sendMoneyToAccount(accountTo, 100);
+			accountTo.withdrawMoney(1000);
+			
+			System.out.println(Arrays.toString(accountFrom.getTransactions()));
+			System.out.println(Arrays.toString(accountTo.getTransactions()));
+		
+		
+		
+		}
+}
+		
+
