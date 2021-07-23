6f8152a186d36e26972e0117417a1eb595815e79
Kunal Sharma <kunal.sharmaAexample.com>
2021-07-23 14:21:51 +0530

    test ssh connection

--- helloWorld.java
+++ helloWorld.java
@@ -13,11 +13,12 @@ public class helloWorld {
 		
 			Account accountFrom = new Account(1);
 			Account accountTo = new Account(2);
+
 			
 			accountFrom.sendMoneyToAccount(accountTo, 100);
 			accountTo.withdrawMoney(1000);
 			
-			System.out.println(Arrays.toString(accountFrom.getTransactions()));
+			System.out.println(Arrays.toString(accountFrom.getTransactions())    );
 			System.out.println(Arrays.toString(accountTo.getTransactions()));
 		
 		
